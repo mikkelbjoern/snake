21e93b2bf77c7082c1dc4574cf47254de46992aa
Mikkel Goldschmidt <mikkelbjoerngoldschmidt@gmail.com>
2017-01-25 00:11:53 +0100

    Added tail

--- snake.js
+++ snake.js
@@ -1,6 +1,7 @@
 snake = function () {
   this.x = 0;
   this.y = 0;
+  this.tail = [];
 
   this.xSpeed = 20;
   this.ySpeed = 20;
@@ -19,6 +20,25 @@ snake = function () {
   this.update = function(){
     this.x = constrain(this.x + this.xSpeed,0,width-20);
     this.y = constrain(this.y + this.ySpeed,0, height-20);
-    this.show()
+    this.moveTail();
+    this.showTail();
+    this.show();
+  }
+
+  this.moveTail = function(){
+    if (!(a.eaten())){
+      this.tail.pop();
+    }
+    this.tail.unshift([this.x,this.y]);
+  }
+
+  this.showTail = function(){
+    for (var i=0; i < this.tail.length; i++){
+      var x = this.tail[i][0];
+      var y = this.tail[i][1];
+      console.log(x,y)
+      fill('#92f442');
+      rect(x,y,20,20);
+    }
   }
 }
